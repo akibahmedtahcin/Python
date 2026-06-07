49265269a0ae728f2eb63ce6d3be5f82a5f1dc54
Akib Ahmed Tahcin <akibahmedtahcin5@gmail.com>
2026-06-07 12:27:38 +0600

    Solved Chapter 02 practice problems

---
+++ Chapter 2 - Strings & Type Conversion/Dynamic Message (f-string).py
@@ -0,0 +1,3 @@
+city = input("Enter city name: ")
+year = input("Enter year: ")
+print(f"I am visiting {city} in {year}.")
---
+++ Chapter 2 - Strings & Type Conversion/String Slicing.py
@@ -0,0 +1,3 @@
+word = "Programming"
+print("First 3 chars:", word[0:3])  # output: Pro
+print("Last 3 chars:", word[-3:])   # output: ing
