c9472f85ef126ed0ec97f9175a1ef9c8cb8ebbf9
Akib Ahmed Tahcin <akibahmedtahcin5@gmail.com>
2026-06-07 11:21:41 +0600

    My first upload

---
+++ Chapter 1/first.py
@@ -0,0 +1,9 @@
+# Defining variables
+name = "Akib"
+age = 22
+height = 5.9
+
+# Checking types
+print(type(name))   # Output: <class 'str'>
+print(type(age))    # Output: <class 'int'>
+print(type(height)) # Output: <class 'float'>
---
+++ Chapter 1/main.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("Welcome to Python programming.")
+print("My name is Akib Ahmed Tahcin.") 
+AkibAhmedTahcin="Akib Ahmed Tahcin" # pascel case
+akibahmedTahcin = "Akib Ahmed Tahcin" # camel case
+akib_ahmed_tahcin = "Akib Ahmed Tahcin" # snake case
+
+a = -12
+b= 3.14 
+c=12/3
+print(type(b)) # float
+print(type(c)) # float
